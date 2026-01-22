ac6c090cc583c43e3558afdef99bbd80dad2b629
زكريا السطان | Zkarya Al-sultan <zico2001m@gmail.com>
2026-01-23 00:46:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ SUPABASE_URL = os.getenv(
     "SUPABASE_URL",
     "https://nophyetcritlguostfsh.supabase.co"
 )
-SUPABASE_KEY = os.getenv(
-    "SUPABASE_KEY",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vcGh5ZXRjcml0bGd1b3N0ZnNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzIyMjEwOCwiZXhwIjoyMDc4Nzk4MTA4fQ.Qp__xP_xX_b2TfwBNZ5TK666zjtd7Yr5Yjp48DOtkmY"
-)
+# SUPABASE_KEY = os.getenv(
+#     "SUPABASE_KEY",
+#     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vcGh5ZXRjcml0bGd1b3N0ZnNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzIyMjEwOCwiZXhwIjoyMDc4Nzk4MTA4fQ.Qp__xP_xX_b2TfwBNZ5TK666zjtd7Yr5Yjp48DOtkmY"
+# )
 
 MODEL_PATH = "news_model.pkl"
 VECTORIZER_PATH = "vectorizer.pkl"
